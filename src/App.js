d5088e339ae299d04b2c42f5fd99605943ed9344
yezinnie14 <yezinnie14@gamil.com>
2025-12-26 18:40:17 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,23 @@
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Layout from "./layout/Layout";
 import MainPage from "./pages/MainPage";
 import CartPage from "./pages/CartPage";
 import CategoryPage from "./pages/CategoryPage";
-import DetailPage from "./pages/DetailPage";
-import { useState } from "react";
 import "./assets/scss/global.scss";
 
 import { useState } from "react";
-import DetailPage from "./pages/DetailPage";
 const App = () => {
-  const [cartItems, setCartItems] = useState([
-    {
-      id: 1,
-      title: "테스트 상품",
-      price: 80000,
-      quantity: 1,
-      image: "shoes01-1.jpg",
-    },
-    {
-      id: 2,
-      title: "테스트 상품2",
-      price: 129000,
-      quantity: 2,
-      image: "shoes01-1.jpg",
-    },
-  ]);
 
-  const onUpdateQty = (id, delta) => {
-    setCartItems((prev) =>
-      prev.map((item) =>
-        item.id === id
-          ? { ...item, quantity: Math.max(1, item.quantity + delta) }
-          : item
-      )
+  const [cartItems, setCartItems] = useState([]);
+
+  const handleAddToCart = (product) => {
+    const existItem = cartItems.find((item) => item.id === product.id);
+
+    if (existItem){
+      const items = cartItems.map((cart)=>
+        cart.id === [product].id ? { ...cart,quantity: cart.quantity +1}
+      : cart
     );
     setCartItems(items);
     } else{
@@ -42,13 +25,20 @@ const App = () => {
       setCartItems(items);
     }
   };
-  //  const handleCartDelete = (id) => {
-  //   const items = cartItems.filter((item) => item.id !== id);
-  //   setCartItems(items);
-  // };
 
-  const onDelete = (id) => {
-    setCartItems((prev) => prev.filter((item) => item.id !== id));
+
+  const handleUpdateQuantity = (id, type) => {
+    const update = cartItems.map((item)=> {
+      if(item.id === id) {
+        if(type === "plus"){
+          return {...item, quantity: item.quantity +1};
+        } else if ( type === "minus" && item.quantity > 1) {
+          return { ...item, quantity: item.quantity - 1};
+        }
+      }
+      return item;
+    });
+    setCartItems(update);
   };
   const handleCartDelete = (id)=>{
   const items = cartItems.filter((item)=>{
@@ -59,41 +49,18 @@ const App = () => {
 
 
 
-  // const onUpdateQty = (id, delta) => {
-  //   setCartItems((prev) =>
-  //     prev.map((item) =>
-  //       item.id === id
-  //         ? { ...item, quantity: Math.max(1, item.quantity + delta) }
-  //         : item
-  //     )
-  //   );
-  // };
-
-  // // ✅ 삭제
-  // const onDelete = (id) => {
-  //   setCartItems((prev) => prev.filter((item) => item.id !== id));
-  // };
-
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<MainPage />} />
-          <Route
-            path="/cart"
-            element={
-              <CartPage
-                cartItems={cartItems}
-                onUpdateQty={onUpdateQty}
-                onDelete={onDelete}
-              />
-            }
-          />
-          <Route path="/category" element={<CategoryPage />} />
-          <Route path="/detail/:id" element={<DetailPage />} />
-        </Route>
+        <Route element={<Layout />} />
+        <Route path="/" element={<MainPage onAdd={handleAddToCart}/>} />
+        <Route path="/cart" element={<CartPage
+          cartItems={cartItems}
+          onUpdate={handleUpdateQuantity}
+          onDelete={handleCartDelete} />} />
+        <Route path="/category" element={<CategoryPage  onAdd={handleAddToCart}/>} />        
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
